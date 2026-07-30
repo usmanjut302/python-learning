2021c3aa0693a47bcc0ac0b6c824a7116ab4a6c2
usmanjut302 <uj786786302@gmail.com>
2026-07-30 10:10:25 +0500

    python day7.py

---
+++ day7.py
@@ -0,0 +1,75 @@
+# fruits=["mnago","orange","kino","bnana","apple"]
+# print(fruits[0])
+# print(fruits[4])
+
+
+# negative slicing
+# fruits=["mnago","orange","kino","bnana","apple"]
+# print(fruits[-1])
+
+# list slicing
+# fruits=["mnago","orange","kino","bnana","apple"]
+# print(fruits[1:3])
+
+
+# problem
+
+# numbers = [10,20,30,40,50,60]
+# print(numbers[:3])
+# print(numbers[3:])
+# print(numbers[3:5])
+
+
+# tuples
+
+# practice 1
+# t=(10,20,30)
+# print(t[1])
+
+# t=(5,)
+# print(t)
+
+# Dictionaries
+# car= {
+#     "brand":"toyota",
+#     "year":2022
+# }
+# print(car["brand"])
+
+# # sets
+
+# num={1,2,4,4,4,6,7,8}
+# print(num)
+# print(len(num))
+
+
+# coding
+
+# 1
+
+# fruits=("mnago","orange","kino","bnana","apple")
+# print(fruits[0])
+# print(fruits[4])
+# print(len(fruits))
+
+
+
+# 2 Dictionaries
+# students={
+#     "name":"usman",
+#     "age":23,
+#     "course":"python"
+# }
+# students["city"]="gojra"
+# print(students["name"])
+# print(students["course"])
+# print(students["age"])
+# print(students["city"])
+
+# 3 Sets
+
+num={1,2,2,3,4,4,5}
+print(num)
+num.add(6)
+num.remove(2)
+print(num)
